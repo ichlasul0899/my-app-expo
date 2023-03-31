69fe74c93f12c085d652ac8939a8f62e3d44107a
Ichlasul Amal <ichlasul0899@gmail.com>
2023-03-31 17:52:55 +0700

    Change Icons Tab Navigation

--- app/home/_layout.js
+++ app/home/_layout.js
@@ -1,18 +1,28 @@
 import { Tabs } from "expo-router";
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign, Entypo } from '@expo/vector-icons';
 
 export default () => {
-    return <Tabs screenOptions={{
+    return <Tabs style={{border: "1px solid red"}} screenOptions={{
         tabBarShowLabel: true,
-        tabBarActiveTintColor: "red"
+        tabBarActiveTintColor: "white",
+        tabBarActiveBackgroundColor: "burlywood",
+        tabBarInactiveBackgroundColor: "burlywood",
+
+        
     }}>
         <Tabs.Screen name="index" options={{
             headerShown: false,
             title: "Home",
-            tabBarIcon: ({ color }) => <AntDesign name="pluscircle" size={24} color={color} />
+            tabBarIcon: ({ color }) => <Entypo name="home" size={24} color={color} />
         }} />
-        <Tabs.Screen name="cart" options={{ headerShown: false }} />
-        <Tabs.Screen name="listItem" options={{ headerShown: false }} />
+        <Tabs.Screen name="cart" options={{ 
+            headerShown: false,
+            title: "Cart",
+            tabBarIcon: ({ color }) => <Entypo name="shopping-cart" size={24} color={color} />  }} />
+        <Tabs.Screen name="listItem" options={{ 
+            headerShown: false,
+            title: "Item",
+            tabBarIcon: ({ color }) => <AntDesign name="heart" size={24} color={color} /> }}/>
         <Tabs.Screen name="profile" options={{ 
             headerShown: false, 
             title: "Profile",
